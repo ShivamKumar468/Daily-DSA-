1d071e29aeaf0a59056ff64c3e6b1350c314986a
Shivam Kumar <shivamkumar@Shivams-MacBook-Air-2.local>
2026-06-28 21:06:27 +0530

    Updated project

--- Day 7/binomial_ncr.cpp
+++ Day 7/binomial_ncr.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
 using namespace std;
 
-void fun(int n,int r){
-    cin>>n;
-    cin>>r;
+int fun(int n){
     int f=1;
     for(int i=1; i<=n; i++){
         f*=i;
     }
+    return f;
 }
+int ncr(int n,int r ){
+    int factorial_n=fun(n);
+    int factorial_r=fun(r);
+    int factorial_nmr=fun(n-r);
 
+    return factorial_n/(factorial_r*factorial_nmr);
+
+}
 int main(){
+    int n=6; int r=5;
+    int result= ncr( n, r);
+    cout<<result;
 
 }
